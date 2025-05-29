5c61b216b4ae8583cdb721c1ad14bba84abc2a6c
Luka Kikvidze <programming1725@gmail.com>
2025-05-29 18:23:41 +0400

    Partners language

--- src/app/pages/partners/partners.module.ts
+++ src/app/pages/partners/partners.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PartnerComponent } from './partners.component';
+import {TranslatePipe} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [PartnerComponent],
-  imports: [CommonModule],
+    imports: [CommonModule, TranslatePipe],
   exports: [PartnerComponent]
 })
 export class PartnerModule { }
