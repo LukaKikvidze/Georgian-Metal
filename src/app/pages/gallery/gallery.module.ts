5946466eb1a015f31a984872103b83457807de10
Luka Kikvidze <programming1725@gmail.com>
2025-05-29 18:29:46 +0400

    Add gallery language

--- src/app/pages/gallery/gallery.module.ts
+++ src/app/pages/gallery/gallery.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { GalleryComponent } from './gallery.component';
 import { PhotoModule } from '../photo/photo.module';
 import { VideoModule } from '../video/video.module';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [GalleryComponent],
   imports: [
     CommonModule,
     PhotoModule,
-    VideoModule
+    VideoModule,
+    TranslatePipe
   ],
   exports: [GalleryComponent]
 })
