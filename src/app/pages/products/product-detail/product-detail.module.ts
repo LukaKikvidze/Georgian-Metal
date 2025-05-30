0cae18b1ca65a8d987abd37041ddff68620aba1d
Luka Kikvidze <programming1725@gmail.com>
2025-05-30 16:55:38 +0400

    Last Update Language

--- src/app/pages/products/product-detail/product-detail.module.ts
+++ src/app/pages/products/product-detail/product-detail.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './product-detail.component';
 import { RouterModule } from '@angular/router';
+import {TranslatePipe} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [ProductDetailComponent],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        TranslatePipe
+    ],
   exports: [ProductDetailComponent]
 })
 export class ProductDetailModule { }
