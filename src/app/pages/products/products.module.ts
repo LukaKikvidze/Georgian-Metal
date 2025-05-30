40617ebdb1f7fc9914b8826dc18ae64168ab69ec
Luka Kikvidze <programming1725@gmail.com>
2025-05-30 16:43:01 +0400

    Add products language

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { RouterModule } from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ProductsComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    TranslatePipe
   ],
   exports: [ProductsComponent]
 })
